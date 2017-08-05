b2ff4caff984bf06fee18f25303a29f77b473b3d
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-05 10:17:03 -0500

    Added targets to package manifest

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "Thread"
+    name: "Thread",
+    targets: [
+        .target(name: "Thread", dependencies: [])
+    ]
 )
