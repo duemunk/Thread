64edcd23e3e1d0073a012c293e8beb8965983ef7
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-05 10:20:26 -0500

    Added products section to package manifest

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "Thread",
-    targets: [
-        .target(name: "Thread", dependencies: [])
-    ]
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Thread",
+            targets: ["Thread"]),
+        ]
 )
